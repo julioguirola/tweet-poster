ad15a49ea9dd867714d0be199490958fd6a37f8e
julioguirola <julioguirola03@gmail.com>
2024-06-09 17:04:40 -0400

    v1.4

--- src/app/up_data/route.js
+++ src/app/up_data/route.js
@@ -1,15 +1,17 @@
 import { getQuotes, getEvents, delAllContent, updateContent } from "@/services/db"
 import { scrap } from "@/services/scrapper"
 
-export async function POST (req) {
+export async function POST () {
   try {
-  	const cubadebateNews = await scrap("http://www.cubadebate.cu", "#front-list > div > .title")
+    console.log("scrapeando cubadebate")
+    const cubadebateNews = await scrap("http://www.cubadebate.cu", "#front-list > div > .title")
+    console.log("scrapeando tvavila")
     const tvCav = await scrap('https://www.tvavila.icrt.cu/', "article h4")
     const quotes = await getQuotes()
     const events = await getEvents()
     await delAllContent()
     await updateContent([...cubadebateNews, ...tvCav, ...quotes, ...events])
-
+    console.log("actualizado")
     return Response.json({ msg: "ok" })
   } catch (e) {
     console.log(e.message)
